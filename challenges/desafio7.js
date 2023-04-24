fde2b379cbc31b175c7f65daf85ebf32c56b8311
Amanda Portela <mandaptela@gmail.com>
2023-04-24 18:32:52 -0300

    lint ok

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,1 +1,3 @@
-db.produtos.find({ $and: [{ curtidas: { $ne: 50 } }, { tags: { $exists: false } }] }, { nome: 1, vendidos: 1, _id: false });
+db.produtos.find({
+  $and: [{ curtidas: { $ne: 50 } }, { tags: { $exists: false } }],
+}, { nome: 1, vendidos: 1, _id: false });
